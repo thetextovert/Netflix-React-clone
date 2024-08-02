55972f6d9fde231c6b4498432e4af8b3b6d42547
Ishani Bansal <ishani.bansal01@nagarro.com>
2024-08-02 16:28:45 +0530

    initial config commit

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-4xl text-center text-green-500 font-bold">
+      <p>Hello People</p>
+    </div>
+  );
+}
+
+export default App;
